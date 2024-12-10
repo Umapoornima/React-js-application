6dd14c033669877dc52a3e105001d5901c27cd77
Umapoornima <111286960+Umapoornima@users.noreply.github.com>
2024-12-10 16:02:45 +0530

    Update App.js

--- az-demo/src/App.js
+++ az-demo/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hellow World from React JS!!!
+          Hellow World from React JS!
         </p>
         <a
           className="App-link"
